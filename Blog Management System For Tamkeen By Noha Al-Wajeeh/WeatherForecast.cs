dad20a9826468046ef07cba9e02c6f00d892de8d
NohaAlwajeeh <59328495+NohaAlwajeeh@users.noreply.github.com>
2024-02-13 09:31:44 +0300

    ada
    commit

---
+++ Blog Management System For Tamkeen By Noha Al-Wajeeh/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Blog_Management_System_For_Tamkeen_By_Noha_Al_Wajeeh
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
